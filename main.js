823ddd389307245ec9982e23b779f57d3f8eaef0
Joseph Jungermann <josephjungermann@Josephs-MBP.home>
2020-03-25 21:04:31 -0400

    added warning and error text in js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const budget_total = parseFloat($(".total_budget span").text());
-//using this code line above to store our value of 1000 on our page after functions kick in.
+//This code line above stores our value of 100 on our page after functions kick in.
 
 $("body").on("keyup keydown keypress change", ".department input", function (e) {
 	let running_total = budget_total;
@@ -7,28 +7,36 @@ $("body").on("keyup keydown keypress change", ".department input", function (e) 
 	$(".department input").each(function () {
 		let user_input = $(this).val();
 		//$(this).val(); means that you are looking for whatever value the user inputs into the field.
-
-		if (user_input === "") {
-			return false;
+		if (user_input !== "") {
+			user_input = parseFloat(user_input);
+			running_total = running_total - user_input;
 		}
-		//the 'if' statement above is used so that when you enter a number in a section while the other sections are blank, then it will still change the budget number up top and not return the NaN on the page.
-
-		user_input = parseFloat(user_input);
-
-		running_total = running_total - user_input;
-		//type --> console.log(running_total); to make sure your running_total is correct.
+		//The 'if' statement above is used so that when you enter a number in a section while the other sections are blank, then it will still change the budget number up top and not return NaN on the page.
 	});
 
-	if (running_total >= 1 && running_total <= 20) {
+	if (running_total >= 0 && running_total <= 20) {
 		$(".total_budget").addClass("warning").removeClass("error");
-	} else if (running_total < 1) {
+	} else if (running_total < 0) {
 		$(".total_budget").addClass("error").removeClass("warning");
 	} else {
 		$(".total_budget").removeClass("error warning");
 	}
-	//This entire block of code starting at the 'if' statement is for CSS and changing the color of the running_total to yellow when the budget hits 20 and red when it hits 0. We use the addClass and removeClass functions so that the colors reset if we make changes in our input fields.
+	//This 'if' block is for CSS and changing the color of the running_total to yellow when the budget hits 20 and red when it hits 0. We use the addClass and removeClass functions so that the colors reset if we make changes in our input fields.
+	let warning_content_text = "You are getting close to budget. Make good choices."
+	let error_content_text = "You are over budget! Make some changes."
+
+	if (running_total >= 0 && running_total <= 20) {
+		$(".warning_content").text(warning_content_text);
+		$(".error_content").text("");
+	} else if (running_total < 0) {
+		$(".error_content").text(error_content_text);
+		$(".warning_content").text("");
+	} else if (running_total > 20) {
+		$(".warning_content").text("");
+		$(".error_content").text("");
+	};
+	//The 'if' block above creates warning and error text when the running total falls within a certain range. It also removes the text string if the budget is changed.
 
 	$(".total_budget span").text(running_total);
-
 
 });
